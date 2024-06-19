b779ec7c9ad9566233926e1043e38602ea4af6c0
Jelmer Firet <spaanse.aak@gmail.com>
2024-06-19 09:32:36 +0200

    Proof for second projection

--- theories/FundamentalLemma.v
+++ theories/FundamentalLemma.v
@@ -195,6 +195,47 @@ Proof.
   erewrite IHl. f_equal. lia.
 Qed.
 
+Fixpoint Proj2Nc (n k : nat) (l : list term) : list term :=
+match l with
+  | nil => nil
+  | x :: xs => (Proj2N n (k + length xs) x) :: (Proj2Nc n k xs)
+end.
+
+Lemma Proj2Nc_0k (k : nat) (l : list term)
+: Proj2Nc 0 k l = l.
+Proof.
+  induction l; simpl. { reflexivity. }
+  rewrite IHl, proj2N_0k. reflexivity.
+Qed.
+
+Lemma Proj2Nc_length (n k : nat) (l : list term)
+: length (Proj2Nc n k l) = length l.
+Proof.
+  induction l; simpl; lia.
+Qed.
+
+Lemma Proj2Nc_nth (n m k : nat) (l : list term) (len : n < length (Proj2Nc m k l)) (len2 : n < length l)
+: safe_nth n (Proj2Nc m k l) len = Proj2N m (k + length l - n - 1) (safe_nth n l len2).
+Proof.
+  revert n m k len len2.
+  induction l; intros n m k len len2;
+  simpl in *; try lia.
+  destruct n; simpl in *. { f_equal. lia. }
+  erewrite IHl. f_equal. lia.
+Qed.
+
+Definition PC_length1 {m n Γ Δ} (eqΓ : length Γ = m) (eqΔ : length Δ = m) (isdef : n < length (PackContext m Γ Δ eqΓ eqΔ))
+: n < length Γ.
+Proof.
+  rewrite PackContext_length in isdef. lia.
+Defined.
+
+Definition PC_length2 {m n Γ Δ} (eqΓ : length Γ = m) (eqΔ : length Δ = m) (isdef : n < length (PackContext m Γ Δ eqΓ eqΔ))
+: n < length Δ.
+Proof.
+  rewrite PackContext_length in isdef. lia.
+Defined.
+
 Lemma PackContext_nth (m n : nat) (Γ Δ : context) (eqΓ : length Γ = m) (eqΔ : length Δ = m)
   (isdef : n < length (PackContext m Γ Δ eqΓ eqΔ)) (isdefΓ : n < length Γ) (isdefΔ : n < length Δ)
 : safe_nth n (PackContext m Γ Δ eqΓ eqΔ) isdef = Pack (Proj1N (m - n - 1) 0 (safe_nth n Γ isdefΓ)) (Proj2N (m - n - 1) 0 (safe_nth n Δ isdefΔ)).
@@ -204,6 +245,13 @@ Proof.
   - simpl in isdefΓ. lia.
   - destruct n; simpl. { f_equal; f_equal; lia. }
     apply IHm.
+Qed.
+
+Lemma PackContext_nth' (m n : nat) (Γ Δ : context) (eqΓ : length Γ = m) (eqΔ : length Δ = m)
+  (isdef : n < length (PackContext m Γ Δ eqΓ eqΔ)) (isdefΓ : n < length Γ) (isdefΔ : n < length Δ)
+: safe_nth n (PackContext m Γ Δ eqΓ eqΔ) isdef = Pack (Proj1N (m - n - 1) 0 (safe_nth n Γ (PC_length1 eqΓ eqΔ isdef))) (Proj2N (m - n - 1) 0 (safe_nth n Δ (PC_length2 eqΓ eqΔ isdef))).
+Proof.
+  apply PackContext_nth.
 Qed.
 
 Lemma lift_proj1 (n m k i : nat) (t : term) (i_lt_k : i < S k)
@@ -289,20 +337,20 @@ Proof.
   remember (Ψ;;Γ;;Ξ) as ΓΞ.
   intro tA.
   revert n k Γ Δ Ξ HeqΓΞ eqΓ eqΔ eqΞ. induction tA; intros m k Γ' Δ Ξ HeqΓ'Ξ eqΓ eqΔ eqΞ;
-  subst Γ; simpl Proj1N; simpl in *; eauto using typed.
-  - apply tyProd. eauto.
+  subst Γ; simpl Proj1N; simpl in *; eauto using ityped.
+  - apply ityProd. eauto.
     replace k with (0 + length Ξ) by lia.
     replace (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;Proj1Nc m 0 Ξ,,Proj1N m (0 + length Ξ) A) with (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;(Proj1Nc m 0 Ξ,,Proj1N m (0 + length Ξ) A)) by reflexivity.
     replace (Proj1Nc m 0 Ξ,,Proj1N m (0 + length Ξ) A) with (Proj1Nc m 0 (Ξ,,A)) by reflexivity.
     apply IHtA2; simpl; eauto.
-  - apply tySum. eauto.
+  - apply itySum. eauto.
     replace k with (0 + length Ξ) by lia.
     replace (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;Proj1Nc m 0 Ξ,,Proj1N m (0 + length Ξ) A) with (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;(Proj1Nc m 0 Ξ,,Proj1N m (0 + length Ξ) A)) by reflexivity.
     replace (Proj1Nc m 0 Ξ,,Proj1N m (0 + length Ξ) A) with (Proj1Nc m 0 (Ξ,,A)) by reflexivity.
     apply IHtA2; simpl; eauto.
   - comp_cases.
     + eassert (Proj1N m k (lift (S n) 0 (safe_nth n (Ψ;;Γ';;Ξ) isdef)) = _).
-      2: { rewrite H. apply tyRel. }
+      2: { rewrite H. apply ityRel. }
       erewrite safe_nth_concat, safe_nth_concat.
       erewrite Proj1Nc_nth.
       rewrite lift_proj1 by lia.
@@ -311,9 +359,9 @@ Proof.
       rewrite app_length, app_length, Proj1Nc_length, PackContext_length. lia.
       lia.
       rewrite Proj1Nc_length. lia.
-    + eapply tyPi1.
+    + eapply ityPi1.
       eassert (∑Proj1N m k (lift (S n) 0 (safe_nth n (Ψ;;Γ';;Ξ) isdef)), _ = _).
-      2: { rewrite H. eapply tyRel. }
+      2: { rewrite H. eapply ityRel. }
       assert (eq1 : n = n - k + length Ξ) by lia.
       assert (eq2 : n = n - k + length (Proj1Nc m 0 Ξ)).
       { rewrite Proj1Nc_length. assumption. }
@@ -335,7 +383,7 @@ Proof.
       rewrite PackContext_length. lia.
       lia. lia.
     + eassert (Proj1N m k (lift (S n) 0 (safe_nth n (Ψ;;Γ';;Ξ) isdef)) = _).
-      2: { rewrite H. apply tyRel. }
+      2: { rewrite H. apply ityRel. }
       assert (isdef' : n < length (Ψ ;; Γ' ;; Ξ)) by assumption.
       rewrite app_length, app_length in isdef'.
       assert (eq1 : n = n - k - m + length Γ' + length Ξ) by lia.
@@ -352,7 +400,7 @@ Proof.
       repeat rewrite app_length in *. lia.
       lia.
       rewrite PackContext_length, app_length, PackContext_length. lia.
-  - apply tyLam with s1 s2; eauto.
+  - apply ityLam with s1 s2; eauto.
     + replace k with (0 + length Ξ) by lia.
       replace (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;Proj1Nc m 0 Ξ,,Proj1N m (0 + length Ξ) A) with (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;(Proj1Nc m 0 Ξ,,Proj1N m (0 + length Ξ) A)) by reflexivity.
       replace (Proj1Nc m 0 Ξ,,Proj1N m (0 + length Ξ) A) with (Proj1Nc m 0 (Ξ,,A)) by reflexivity.
@@ -364,13 +412,13 @@ Proof.
   - replace k with (k + 0) by lia. rewrite Proj1N_subst.
     replace (k + 0) with k by lia.
     replace (S k + 0) with (S k) by lia.
-    eapply tyApp; eauto.
+    eapply ityApp; eauto.
 
     replace k with (0 + length Ξ) by lia.
       replace (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;Proj1Nc m 0 Ξ,,Proj1N m (0 + length Ξ) A) with (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;(Proj1Nc m 0 Ξ,,Proj1N m (0 + length Ξ) A)) by reflexivity.
       replace (Proj1Nc m 0 Ξ,,Proj1N m (0 + length Ξ) A) with (Proj1Nc m 0 (Ξ,,A)) by reflexivity.
       eauto.
-  - eapply tyPair; eauto.
+  - eapply ityPair; eauto.
     + replace k with (0 + length Ξ) by lia.
       replace (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;Proj1Nc m 0 Ξ,,Proj1N m (0 + length Ξ) A) with (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;(Proj1Nc m 0 Ξ,,Proj1N m (0 + length Ξ) A)) by reflexivity.
       replace (Proj1Nc m 0 Ξ,,Proj1N m (0 + length Ξ) A) with (Proj1Nc m 0 (Ξ,,A)) by reflexivity.
@@ -382,14 +430,14 @@ Proof.
   - replace k with (k + 0) by lia. rewrite Proj1N_subst.
     replace (k + 0) with k by lia.
     replace (S k + 0) with (S k) by lia.
-    eapply tyPi2. eauto.
+    eapply ityPi2. eauto.
   - replace k with (k + 0) by lia.
     repeat rewrite Proj1N_subst.
     replace (S k) with (1 + k) by lia. rewrite <-lift_proj1 by lia.
     replace (S (1 + k)) with (2 + k) by lia. rewrite <- lift_proj1 by lia.
     replace (k + 0) with k by lia.
     replace (2 + k) with (S (S k)) by lia.
-    eapply tyJ.
+    eapply ityJ.
     + specialize (IHtA1 m (S (S (S k))) Γ' Δ (Ξ,,A,,lift 1 0 A,,^1 == ^0)).
       simpl Proj1Nc in IHtA1.
       replace (S (length Ξ)) with (1 + length Ξ) in IHtA1 by lia.
@@ -406,6 +454,236 @@ Proof.
     + rewrite eqΞ. eauto.
     + rewrite eqΞ. eauto.
     + eauto.
-  - eapply tyConv; eauto.
+  - eapply ityConv; eauto.
     apply Proj1N_conv. assumption.
 Qed.
+
+Lemma lift_proj2 (n m k i : nat) (t : term) (i_lt_k : i < S k)
+: lift n i (Proj2N m k t) = Proj2N m (n + k) (lift n i t).
+Proof.
+  revert m k i i_lt_k. induction t; intros m k i i_lt_k; unfold lift;
+  simpl in *; f_equal; eauto.
+  - unfold skip, jump. comp_cases.
+  - subst_helper. replace (S (n + k)) with (n + S k) by lia. apply IHt2. lia.
+  - subst_helper. replace (S (n + k)) with (n + S k) by lia. apply IHt. lia.
+  - subst_helper. replace (S (n + k)) with (n + S k) by lia. apply IHt2. lia.
+  - subst_helper. replace (S (n + k)) with (n + S k) by lia. apply IHt1. lia.
+Qed.
+
+Lemma Proj2N_lift (n m k1 k2 i : nat) (t : term) (k1m : k1 <= m) (k2m : k2 <= m)
+: Proj2N (n-k1) (i + k1) (lift m i t) = Proj2N (n-k2) (i + k2) (lift m i t).
+Proof.
+  revert i; induction t; intros i;
+  unfold lift; simpl; f_equal; subst_helper; 
+  try replace (S (i + k1)) with ((i + 1) + k1) by lia;
+  try replace (S (i + k2)) with ((i + 1) + k2) by lia;
+  eauto.
+  unfold skip, jump. comp_cases.
+Qed.
+
+Lemma Proj2N_lift' (m n k i : nat) (t : term) (nmk : m + k < n)
+: Proj2N m (k + i) (lift n i t) = lift n i t.
+Proof.
+  revert i k nmk; induction t; intros i k nmk;
+  unfold lift; simpl; subst_helper; f_equal;
+  try replace (S (k + i)) with (k + (i + 1)) by lia;
+  eauto.
+  unfold skip, jump. comp_cases.
+Qed.
+
+Lemma Proj2N_subst (m k i : nat) (t u : term)
+: Proj2N m (k + i) t[i ← u] = (Proj2N m (S k + i) t)[i ← Proj2N m k u].
+Proof.
+  revert m k i u. induction t; intros m k i u;
+  simpl in *; f_equal; eauto;
+  try replace (S (k + i)) with (k + S i) by lia;
+  try replace (S (k + S i)) with (S k + S i) by lia; eauto.
+  comp_cases; simpl; comp_cases.
+  rewrite lift_proj2 by lia.
+  f_equal. lia.
+Qed.
+
+Lemma Proj2N_red1 (n k : nat) (u v : term)
+: u ▷ v -> Proj2N n k u ▷ Proj2N n k v.
+Proof.
+  intro uv.
+  revert k; induction uv; intro k; simpl; eauto using red1.
+  - replace k with (k + 0) by lia.
+    rewrite Proj2N_subst.
+    replace (k + 0) with k by lia.
+    replace (S k + 0) with (S k) by lia.
+    apply red1_beta_app.
+  - replace k with (k + 0) by lia.
+    rewrite Proj2N_subst.
+    replace (k + 0) with k by lia.
+    replace (S k + 0) with (S k) by lia.
+    apply red1_beta_J.
+  - replace (S k) with (1 + k) by lia.
+    rewrite <- lift_proj2 by lia.
+    apply red1_eta_lambda.
+Qed.
+
+Lemma Proj2N_conv (n k : nat) (u v : term)
+: u ≡ v -> Proj2N n k u ≡ Proj2N n k v.
+Proof.
+  intro uv. induction uv.
+  - apply conv_refl.
+  - eapply conv_red_l. 2: { eassumption. }
+    apply Proj2N_red1. assumption.
+  - eapply conv_red_r. 2: { eassumption. }
+    apply Proj2N_red1. assumption.
+Qed.
+
+Lemma Pack2 {n k} (Γ Δ Ξ Ψ : context) (eqΓ : length Γ = n) (eqΔ : length Δ = n) (eqΞ : length Ξ = k) (t A : term)
+: Ψ ;; Δ ;; Ξ ⊢ᵢ t : A ->
+  Ψ ;; (PackContext n Γ Δ eqΓ eqΔ) ;; (Proj2Nc n 0 Ξ) ⊢ᵢ Proj2N n k t : Proj2N n k A.
+Proof.
+  remember (Ψ;;Δ;;Ξ) as ΔΞ.
+  intro tA.
+  revert n k Γ Δ Ξ HeqΔΞ eqΓ eqΔ eqΞ. induction tA; intros m k Γ' Δ Ξ HeqΓ'Ξ eqΓ eqΔ eqΞ;
+  subst Γ; simpl Proj2N; simpl in *; eauto using ityped.
+  - apply ityProd. eauto.
+    replace k with (0 + length Ξ) by lia.
+    replace (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A) with (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;(Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A)) by reflexivity.
+    replace (Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A) with (Proj2Nc m 0 (Ξ,,A)) by reflexivity.
+    apply IHtA2; simpl; eauto.
+  - apply itySum. eauto.
+    replace k with (0 + length Ξ) by lia.
+    replace (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A) with (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;(Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A)) by reflexivity.
+    replace (Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A) with (Proj2Nc m 0 (Ξ,,A)) by reflexivity.
+    apply IHtA2; simpl; eauto.
+  - comp_cases.
+    + eassert (Proj2N m k (lift (S n) 0 (safe_nth n (Ψ;;Δ;;Ξ) isdef)) = _).
+      2: { rewrite H. apply ityRel. }
+      erewrite safe_nth_concat, safe_nth_concat.
+      erewrite Proj2Nc_nth.
+      rewrite lift_proj2 by lia.
+      f_equal. lia.
+      Unshelve.
+      rewrite app_length, app_length, Proj2Nc_length, PackContext_length. lia.
+      lia.
+      rewrite Proj2Nc_length. lia.
+    + assert (isdef' : n-k < length (PackContext m Γ' Δ eqΓ eqΔ)). { rewrite PackContext_length. lia. }
+      assert (isdef'' : n-k < length (PackContext m Γ' Δ eqΓ eqΔ)). { rewrite PackContext_length. lia. }
+      set (x := (Proj1N (m + k - n - 1) 0 (safe_nth (n - k) Γ' (PC_length1 eqΓ eqΔ isdef')))).
+      set (y := (Proj2N (m + k - n - 1) 0 (safe_nth (n - k) Δ (PC_length2 eqΓ eqΔ isdef')))).
+      eapply (ityPi1 _ _ (∑(lift (n + 2) 0 x)==(lift (n + 2) 0 y),tTransport ^0 (π₁ ^(n+2))==^1)).
+      eassert (H1 : ∑Proj2N m k (lift (S n) 0 (safe_nth n (Ψ;;Δ;;Ξ) isdef)), _ = _).
+      2: {
+        rewrite H1. eapply ityPi2.
+        eassert (H2 : ∑_, _ = _).
+        2: { rewrite H2. eapply ityRel. }
+        assert (eq : n = n - k + length (Proj2Nc m 0 Ξ)). { rewrite Proj2Nc_length. lia. }
+        rewrite eq.
+        erewrite safe_nth_concat', safe_nth_concat.
+        rewrite <- eq.
+        erewrite PackContext_nth' by lia.
+        replace (m - (n - k) - 1) with (m + k - (n + 1)) by lia.
+        unfold Pack. simpl. subst_helper. simpl "+". f_equal.
+      }
+      replace 1 with (0 + 1) by lia. repeat rewrite lift_lift by lia. replace (0 + 1) with 1 by lia.
+      replace 2 with (0 + 2) by lia. repeat rewrite lift_lift by lia. replace (0 + 2) with 2 by lia.
+      repeat rewrite lift_add.
+      simpl.
+      repeat rewrite subst_lift by lia.
+      f_equal.
+      * rewrite lift_proj2 by lia.
+        replace (n + 1 + 0) with (0 + (n + 1)) by lia.
+        rewrite (Proj2N_lift _ _ _ k) by lia.
+        f_equal. lia.
+        f_equal. f_equal. f_equal. lia.
+        assert (eq : n = n-k+length Ξ) by lia.
+        rewrite (safe_nth_rewrite _ _ _ _ eq).
+        erewrite safe_nth_concat'.
+        erewrite safe_nth_concat.
+        apply safe_nth_proof_irrelevant.
+      * subst x y; f_equal; f_equal; f_equal; f_equal; try lia.
+        f_equal. unfold skip, jump. comp_cases.
+
+      Unshelve.
+      repeat rewrite app_length. rewrite PackContext_length, Proj2Nc_length. lia.
+      repeat rewrite app_length. rewrite PackContext_length. lia.
+      rewrite app_length. lia.
+      lia.
+    + eassert (Proj2N m k (lift (S n) 0 (safe_nth n (Ψ;;Δ;;Ξ) isdef)) = _).
+      2: { rewrite H. apply ityRel. }
+      assert (isdef' : n < length (Ψ ;; Δ ;; Ξ)) by assumption.
+      rewrite app_length, app_length in isdef'.
+      assert (eq1 : n = n - k - m + length Δ + length Ξ) by lia.
+      assert (eq2 : n = n - k - m + length (PackContext m Γ' Δ eqΓ eqΔ) + length (Proj2Nc m 0 Ξ)).
+      { rewrite Proj2Nc_length. rewrite PackContext_length. lia. }
+      rewrite safe_nth_rewrite with _ _ _ _ eq1.
+      rewrite safe_nth_rewrite with _ _ _ _ eq2.
+      erewrite safe_nth_concat', safe_nth_concat'.
+      erewrite safe_nth_concat', safe_nth_concat'.
+      replace (Proj2N m k) with (Proj2N m (k + 0)) by (f_equal; lia).
+      apply Proj2N_lift'. lia.
+      Unshelve.
+      repeat rewrite app_length in *. rewrite Proj2Nc_length. rewrite PackContext_length. lia.
+      repeat rewrite app_length in *. lia.
+      repeat rewrite app_length in *. lia.
+      rewrite PackContext_length, app_length, PackContext_length. lia.
+  - apply ityLam with s1 s2; eauto.
+    + replace k with (0 + length Ξ) by lia.
+      replace (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A) with (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;(Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A)) by reflexivity.
+      replace (Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A) with (Proj2Nc m 0 (Ξ,,A)) by reflexivity.
+      eauto.
+    + replace k with (0 + length Ξ) by lia.
+      replace (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A) with (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;(Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A)) by reflexivity.
+      replace (Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A) with (Proj2Nc m 0 (Ξ,,A)) by reflexivity.
+      eauto.
+  - replace k with (k + 0) by lia. rewrite Proj2N_subst.
+    replace (k + 0) with k by lia.
+    replace (S k + 0) with (S k) by lia.
+    eapply ityApp; eauto.
+
+    replace k with (0 + length Ξ) by lia.
+      replace (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A) with (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;(Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A)) by reflexivity.
+      replace (Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A) with (Proj2Nc m 0 (Ξ,,A)) by reflexivity.
+      eauto.
+  - eapply ityPair; eauto.
+    + replace k with (0 + length Ξ) by lia.
+      replace (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A) with (Ψ;;PackContext m Γ' Δ eqΓ eqΔ;;(Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A)) by reflexivity.
+      replace (Proj2Nc m 0 Ξ,,Proj2N m (0 + length Ξ) A) with (Proj2Nc m 0 (Ξ,,A)) by reflexivity.
+      eauto.
+    + replace (S k) with (S k + 0) by lia.
+      rewrite <- Proj2N_subst.
+      replace (k + 0) with k by lia.
+      eauto.
+  - replace k with (k + 0) by lia. rewrite Proj2N_subst.
+    replace (k + 0) with k by lia.
+    replace (S k + 0) with (S k) by lia.
+    eapply ityPi2. eauto.
+  - replace k with (k + 0) by lia.
+    repeat rewrite Proj2N_subst.
+    replace (S k) with (1 + k) by lia. rewrite <-lift_proj2 by lia.
+    replace (S (1 + k)) with (2 + k) by lia. rewrite <- lift_proj2 by lia.
+    replace (k + 0) with k by lia.
+    replace (2 + k) with (S (S k)) by lia.
+    eapply ityJ.
+    + specialize (IHtA1 m (S (S (S k))) Γ' Δ (Ξ,,A,,lift 1 0 A,,^1 == ^0)).
+      simpl Proj2Nc in IHtA1.
+      replace (S (length Ξ)) with (1 + length Ξ) in IHtA1 by lia.
+      rewrite <- lift_proj2 in IHtA1.
+      replace (S (S (S k)) + 0) with (S (S (S k))) by lia.
+      apply IHtA1; simpl; eauto.
+      lia.
+    + replace (Refl(^1)) with (Proj2N m (S (S k)) Refl(^1)) by reflexivity.
+      replace ^0 with (Proj2N m (S k) ^0) by reflexivity.
+      repeat rewrite <- Proj2N_subst.
+      replace (S k + 0) with (S k) by lia.
+      specialize (IHtA2 m (S k) Γ' Δ (Ξ,,A)).
+      simpl Proj2Nc in IHtA2. apply IHtA2; simpl; eauto.
+    + rewrite eqΞ. eauto.
+    + rewrite eqΞ. eauto.
+    + eauto.
+  - eapply ityConv; eauto.
+    apply Proj2N_conv. assumption.
+Admitted. (* doesn't get through final checks *)
+
+Lemma fundamental_lemma {Γ Γ₁ Γ₂} {t₁ t₂ T₁ T₂ : term} {n : nat} (isdef1 : length Γ₁ = n) (isdef2 : length Γ₂ = n)
+: t₁ ≃ t₂ -> 
+  Γ;;Γ₁ ⊢ᵢ t₁ : T₁ -> 
+  Γ;;Γ₂ ⊢ᵢ t₂ : T₂ ->
+  exists p, Γ ;; (PackContext n Γ₁ Γ₂ isdef1 isdef2) ⊢ᵢ p : Heq (Proj1N n 0 T₁) (Proj2N n 0 T₂) (Proj1N n 0 t₁) (Proj2N n 0 t₂).
+Admitted.
